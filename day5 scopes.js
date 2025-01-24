bcee5c77ac7bf734e842eec199840868a0b2bad4
Balaprasad <kondabalu4@gmail.com>
2025-01-23 21:51:27 -0600

    my new UI course

---
+++ day5 scopes.js
@@ -0,0 +1,19 @@
+let employeename = "shiva"
+let employeeid = 234
+let salary = 50000
+let department = "IT"
+let experience = 5
+let skills = ["Python", "Java", "C++"]
+
+function employeedetails()
+{
+    let designation = "Software Engineer"
+    console.log("Employee Designation:",designation);
+    console.log("Employee Name:", employeename);
+}
+console.log("Employee Salary:",salary)
+console.log("Employee ID:",employeeid)
+
+if true{
+    console.log("Employee Department:",department)
+}
---
+++ day5 types.js
@@ -0,0 +1,19 @@
+let studentname = "Raju";
+let studentage = 24;
+let isStudent = true;
+let studentmarks = 85;
+let studentpercentage = 85.5;
+let studentgrade = "A";
+
+studentage = 25         //this worked
+
+const gradyear = "2024";
+//gradyear = "2025"   //this did not work
+
+console.log("Student Name:",studentname)
+console.log("Student Age:",studentage)
+console.log("Is The Student from this School:",isStudent)
+console.log("Year of Graduation:",gradyear)
+console.log("Student Marks:",studentmarks)
+console.log("Student Percentage:",studentpercentage)
+console.log("Student Grade:",studentgrade)
